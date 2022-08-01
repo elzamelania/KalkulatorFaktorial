43dfdf1cafe7865c8697520f815192b13b20a261
elzamelania <elza@radyalabs.com>
2022-08-01 21:55:47 +0700

    Run test suite

--- Scripts/04 Hitung non Integer/Script1659363010383.groovy
+++ Scripts/04 Hitung non Integer/Script1659363010383.groovy
@@ -17,8 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
 WebUI.navigateToUrl(GlobalVariable.url)
 
 WebUI.click(findTestObject('Page_Faktoriall/btn_Hitung Faktorial'))
--- Test Suites/Suite Kalkulator Faktorial.ts
+++ Test Suites/Suite Kalkulator Faktorial.ts
@@ -11,4 +11,32 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>62183bcf-edd5-4843-b157-f432abeb0003</testSuiteGuid>
+   <testCaseLink>
+      <guid>2584a8f4-a276-4176-9de5-d2a537ef58ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/01 View Page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c13a6e22-4f94-4262-ac87-298732251c29</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/02 Hitung Integer Positif</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>49d0fca5-ea35-4776-aa21-35c568ecbcf6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/03 Hitung 0 Integer Negatif</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8726f926-35d0-4b01-8bc0-9bd01643c0b3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/04 Hitung non Integer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
